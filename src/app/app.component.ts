3c2b9ea7fdb02a27cc90e0edd5b8c1abfaa55803
David Martinez Ros <davidnezan@gmail.com>
2019-06-25 18:22:56 +0200

    resolve the doesnt work jquery issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit  } from '@angular/core';
 import { ReturnJsonArrayService } from './return-json-array.service';
 import { environment } from 'src/environments/environment';
-import { Data } from './data';
 import { Observable } from 'rxjs';
-import { Portlet } from './portlet';
 import { Column } from './column';
+import * as $ from 'jquery';
+window["$"] = $;
+window["jQuery"] = $;
+declare var $;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'bind-json-array-to-draggable-portlets';
 
   data: Observable<Array<Column>>;
 
   constructor(private service: ReturnJsonArrayService) {
-    debugger;
     this.data = this.service.getScreen('..' + environment.directory + '/assets/json/data.json');
     this.data.subscribe(val => console.log(val));
+  }
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.loadJQuery();
+    }, 100);
+  }
+
+  loadJQuery() {
+    $( ".column" ).sortable({
+      connectWith: ".column",
+      handle: ".portlet-header",
+      cancel: ".portlet-toggle",
+      placeholder: "portlet-placeholder ui-corner-all"
+    });
+ 
+    $( ".portlet" )
+      .addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" )
+      .find( ".portlet-header" )
+        .addClass( "ui-widget-header ui-corner-all" )
+        .prepend( "<span class='ui-icon ui-icon-minusthick portlet-toggle'></span>");
+        
+    $( ".portlet-toggle" ).on( "click", function() {
+      var icon = $( this );
+      icon.toggleClass( "ui-icon-minusthick ui-icon-plusthick" );
+      icon.closest( ".portlet" ).find( ".portlet-content" ).toggle();
+    });
   }
 }
